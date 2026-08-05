81136d0bb7eadaf9f6811580ca56f68dcd568a57
Siva Bhavani <pasamsivabhavani@gmail.com>
2026-08-05 15:38:36 +0530

    Updated Marketplace UI and CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CollectionProvider } from "./context/CollectionContext";
+import "./index.css";
 
 ReactDOM.createRoot(
   document.getElementById("root")!
@@ -11,4 +12,4 @@ ReactDOM.createRoot(
       <App />
     </CollectionProvider>
   </React.StrictMode>
-);
+);s
--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useCollection } from "../../context/CollectionContext";
 
 
